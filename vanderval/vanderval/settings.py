618a5d0894007c7a37f37535e5f47424711820d2
Satyam Raj <satyamrajarya@gmail.com>
2025-01-08 17:22:11 +0530

    added website in app

--- vanderval/vanderval/settings.py
+++ vanderval/vanderval/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "website",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    # "website",
 ]
 
 MIDDLEWARE = [
