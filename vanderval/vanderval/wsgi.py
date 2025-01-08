4886d9915791661b6dee46e08b5e2673d1eeccdc
Satyam Raj <satyamrajarya@gmail.com>
2025-01-08 16:36:02 +0530

    added setting module for deployement

--- vanderval/vanderval/wsgi.py
+++ vanderval/vanderval/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanderval.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanderval.vanderval.settings')
 
 application = get_wsgi_application()
